44dfc35173ef56eec14c2a0a26bfd18f61927843
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2024-02-20 10:44:52 +0100

    removed src

--- shopping/index.js
+++ shopping/index.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/', (req, res, next) => {
-	return res.status(200).json({ msg: 'Hello from Shopping' });
+	return res.status(200).json({ msg: 'Hello from Shoppping' });
 });
 
-app.listen(8002, () => {
-	console.log('Products is Listening to Port 8002');
+app.listen(8003, () => {
+	console.log('Shopping is Listening to Port 8008');
 });
