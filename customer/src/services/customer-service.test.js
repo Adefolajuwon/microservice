b918ceda0e56c28457a77fcf8b3056539efc4043
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2024-02-28 21:05:18 +0100

    customer service

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,10 +1,9 @@
 // which service it is
-describe("CustomerService", () => {
-  // This will explain which function we are testing!
-  describe("SignIn", () => {
-    // Which Scenario we are testing
-    test("validate user inputs", () => {});
+describe('CustomerService', () => {
+	describe('SignIn', () => {
+		// Which Scenario we are testing
+		test('validate user inputs', () => {});
 
-    test("Validate response", async () => {});
-  });
+		test('Validate response', async () => {});
+	});
 });
